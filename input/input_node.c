e18abc6e3173635dab5b2103df5f6003639a029a
obirchman <101401873+obirchman@users.noreply.github.com>
2022-04-20 06:15:44 +0200

    Co-authored-by: ShayanG9 <ShayanG9@users.noreply.github.com>

--- input/input_node.c
+++ input/input_node.c
@@ -1,39 +1,72 @@
 #ifndef F_CPU
 #define F_CPU 16000000UL
 #endif
-#ifndef __AVR_ATmega328P__
-#define __AVR_ATmega328P__
+#ifndef AVR_ATmega328P
+#define AVR_ATmega328P
 #endif
 #define BAUD 9600
-
+#define DATA_START (length - 9)
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
 #include <util/setbaud.h>
 
-void USART_init(){	//asynchronous, no parity, 1-stop bit, 8-bit frame
-	UBRR0H = UBRRH_VALUE;
-	UBRR0L = UBRRL_VALUE;
-	UCSR0B = _BV(UCSZ01) | _BV(UCSZ00);
-	UCSR0B = _BV(TXEN0);
+void USART_init(){    //asynchronous, no parity, 1-stop bit, 8-bit frame
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+    UCSR0B = _BV(UCSZ01) | _BV(UCSZ00);
+    UCSR0B = _BV(RXEN0);
 }
 uint8_t USART_receive(){ // receive a single byte, polling.
-	loop_until_bit_is_set(UCSR0A,RXC0);
-	return UDR0;
+    loop_until_bit_is_set(UCSR0A,RXC0);
+    return UDR0;
 }
+
 void USART_send(uint8_t msg){   // send a single byte, polling.
-	loop_until_bit_is_set(UCSR0A,UDRE0);
-	UDR0 = msg;
+    loop_until_bit_is_set(UCSR0A,UDRE0);
+    UDR0 = msg;
 }
-int main(void){
-	USART_init();
-    while(1){
-        uint8_t msg = 'A';
-		USART_send(msg);
-        _delay_ms(1000);
-        msg = 'B';
-        USART_send(msg);
-        _delay_ms(1000);
+
+void wait_for_delimiter(){
+	while(!(USART_receive() == 0x7E));
+}
+void czech_checksum(){
+
+}
+uint8_t startReceive(){
+
+    uint16_t length = 8 << USART_receive();
+     length += USART_receive();
+
+    uint8_t iterator = 0;
+    uint8_t payload[length+1];
+	uint8_t checksum = 0;
+
+	//receive it all
+    while(iterator < length + 1){
+        payload[iterator] = USART_receive();
+		checksum += payload[iterator++];
+    }
+	if(checksum != 0xFF){
+		return -1;
+	}
+	// read data into new array
+    uint8_t data[DATA_START];
+    for(int i = 9; i < length; i++){
+		data[i-9] = payload[i];
 	}
 	return 0;
+} 
+
+int main(void){
+    USART_init();
+    while(1){
+        wait_for_delimiter();
+        uint8_t ret = startReceive();
+		send_verify(ret);
+		/*
+		Do programmy things
+		*/
+	}
+    return 0;
 }
--- lab_xbee.py
+++ lab_xbee.py
@@ -1,17 +1,22 @@
 import sys
 import time
 from digi.xbee.devices import Raw802Device
+from digi.xbee.devices import ZigBeeDevice
+from digi.xbee.models.address import XBee64BitAddress
 
 # In order
 # DELIMITER = '7E'
 # LENGTH TWO BYTES (DATA BYTES + ADDRESS + NO_ACK = DATA + 8 + 1 = DATA + 9)
 # TYPE = '00'
+COORD_MAC = '0013A20041B77570'
 MAC1 = '0013A20041C72100'
 MAC2 = '0013A20041C72075'
 # OPTIONS = '01'  # DISABLE ACK
 # CHECKSUM ONE BYTE
 
-coordinator = Raw802Device("COM1", 9600) # The big boi bee that controls tbe other bees
+PORT = 'COM16'
+BAUD = 9600
+coordinator = Raw802Device(PORT, BAUD) # The big boi bee that controls tbe other bees
 
 
 # After sending to a bee, bee must reply and this checks that the correct bee replied and
@@ -33,23 +38,28 @@ def get_nodes():
     net.start_discovery_process()   # Starts looking for bees
     while net.is_discovery_running():   # Locks until looking it is done looking for bees
         time.sleep(0.5)
-    node1 = net.get_device_by_64(MAC1)  # Get the specific bee
+    
+    node1 = net.get_device_by_64(XBee64BitAddress(bytes.fromhex(MAC1)))  # Get the specific bee
     if node1 is None:   # Make sure it is actually there
         print("Device with 64-bit addr: %s not found" % str(MAC1))
-    node2 = net.get_device_by_64(MAC2)  # Get second bee
-    if node2 is None: 
-        print("Device with 64-bit addr: %s not found" % str(MAC2))
-    return node1,node2  # Return the bees
-def main():
-    e1,e2 = get_nodes()
-    msg = 'A'
-    coordinator.send_data_async(e1,msg) # Sends msg to bee e1
-    verify_reply(e1)
-    
-    msg = 'B'
-    coordinator.send_data_async(e2,msg)  # Sends msg to bee e2
-    verify_reply(e1)
-    
+    # node2 = net.get_device_by_64(XBee64BitAddress(bytes.fromhex(MAC2)))  # Get second bee
+    # if node2 is None: 
+    #   print("Device with 64-bit addr: %s not found" % str(MAC2))
+    return node1  # Return the bees
 
+
+def main():
+    e1 = get_nodes()
+    msg = 0
+    while(1):
+        msg += 1
+        msgbyte = bytes(msg)
+        coordinator.send_data_async(e1,msgbyte) # Sends msg to bee e1
+        time.sleep(1)
+        if(msg == 5):
+            msg = 0
+    return 0
+    
+    
 if __name__ == '__main__':
     sys.exit(main())
